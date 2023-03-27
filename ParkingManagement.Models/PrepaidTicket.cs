e328dfea2a52f2083698b0c4b2ea6d8480aef69d
Grico-Mic <igorgric@outlook.com>
2023-03-27 20:46:14 +0200

    Create a new project for Models,add class "PrepaidTicket" with properties and method.

---
+++ ParkingManagement.Models/PrepaidTicket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ParkingManagement.Models
+{
+    public class PrepaidTicket
+    {
+        public int RegistrationNumber { get; set; }
+        public decimal PricePerMonths { get; set; }
+
+        public DateTime ValidFrom { get; set; }
+        public int MonthsValid { get; set; }
+
+
+        public bool IsValid(DateTime currentDate)
+        {
+            return currentDate < ValidFrom.AddMonths(MonthsValid);
+        }
+    }
+}
