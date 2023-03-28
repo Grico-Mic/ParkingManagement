91b59aa86330017373efb5e1d0bf7d10ac1a6acd
Grico-Mic <igorgric@outlook.com>
2023-03-28 20:21:50 +0200

    Create new project Repositories with method for create the data,and add reference to Models.

---
+++ ParkingManagement.Repositories/PrepaidTicketRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkingManagement.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ParkingManagement.Repositories
+{
+    public class PrepaidTicketRepository
+    {
+        public PrepaidTicketRepository()
+        {
+            Data = new List<PrepaidTicket>();
+        }
+        public List<PrepaidTicket> Data { get; set; }
+
+        public void Create(PrepaidTicket prepaidticket)
+        {
+            Data.Add(prepaidticket);
+        }
+    }
+}
